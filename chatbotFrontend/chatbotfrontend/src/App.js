977d584a72be901028acbed3da435355d3cc89bc
Chris <chrishayes0708@gmail.com>
2022-05-01 19:24:41 -0600

    added chathistory component (doesn't work yet)

--- chatbotFrontend/chatbotfrontend/src/App.js
+++ chatbotFrontend/chatbotfrontend/src/App.js
@@ -3,11 +3,24 @@ import './App.css';
 import React, { Component } from  "react";
 import { connect, sendMsg } from "./api"
 import Header from "./components/Header/Header"
+import ChatHistory from './components/Header/ChatHistory';
 
 class App extends Component{
   constructor(props) {
     super(props)
-    connect();
+    this.state = {
+      chatHistory: []
+    }
+  }
+
+  componentDidMount(){
+    connect((msg) => {
+      console.log("New Message")
+      this.setState(prevState => ({
+        chatHistory: [...this.state.chatHistory, msg]
+      }))
+      console.log(this.state);
+    });
   }
   // send method to send a chat command
   send(){
@@ -20,6 +33,7 @@ class App extends Component{
       
       <div className="App">
         <Header /> 
+        <ChatHistory chatHistory={this.state.chatHistory} />
         <button onClick={this.send}>Hit</button>
       </div>
     )
